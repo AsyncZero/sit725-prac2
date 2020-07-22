40492aac124e8e02c2d91ad7ddec56990c113ffd
Mitch <mboyd@outlook.com.au>
2020-07-22 19:31:47 +1000

    Sentiment example in separate file

--- server-sentiment.js
+++ server-sentiment.js
@@ -1,13 +1,19 @@
 //Requires Express
 const express = require("express");
-const { response } = require("express");
+const Sentiment = require("sentiment");
 
 //Create Express server
 let app = express();
 
-//Create addition function
 let addition = function(num1,num2) {
     let result = num1 + num2;
+    return result;
+}
+
+var sentiment = new Sentiment();
+
+let sentimentAnalysis = function(text) {
+    let result = sentiment.analyze(text);
     return result;
 }
 
@@ -27,10 +33,15 @@ app.get("/test",function(request,response) {
     response.send("Test Endpoint");
 })
 
-//Create Calc endpoint
 app.get("/calc",function(request,response) {
     let num1 = parseInt(request.query.num1);
     let num2 = parseInt(request.query.num2);
     let addResult = addition(num1,num2);
     response.send("Calculator Result: " + addResult);
+})
+
+app.get("/analyze",function(request,response) {
+    let text = request.query.text;
+    let result = sentimentAnalysis(text);
+    response.send(result)
 })
