8dc7ff95cbc321625571455065e038c8080d5442
Mitch <mboyd@outlook.com.au>
2020-07-22 19:14:08 +1000

    Calc Function

---
+++ server.js
@@ -0,0 +1,34 @@
+//Requires Express
+const express = require("express");
+const { response } = require("express");
+
+//Create Express server
+let app = express();
+
+let addition = function(num1,num2) {
+    let result = num1 + num2;
+    return result;
+}
+
+//Create static web page
+app.use(express.static("public"));
+
+//Starts server on port listed (3000)
+app.listen(3000);
+
+//Create root endpoint
+app.get("/",function(request,response) {
+    response.send("Prac 2 - Hello World");
+})
+
+//Create Test endpoint
+app.get("/test",function(request,response) {
+    response.send("Test Endpoint");
+})
+
+app.get("/calc",function(request,response) {
+    let num1 = parseInt(request.query.num1);
+    let num2 = parseInt(request.query.num2);
+    let addResult = addition(num1,num2);
+    response.send("Calculator Result: " + addResult);
+})
